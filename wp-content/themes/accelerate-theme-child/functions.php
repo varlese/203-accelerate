2e1056556dcd318c0089720b797853ba86d8076e
varlese <1361760+varlese@users.noreply.github.com>
2018-11-11 08:50:44 -0500

    Added social plugins and widget area to home page

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -14,6 +14,7 @@
 function accelerate_child_scripts(){
 	wp_enqueue_style( 'accelerate-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' ));
+    wp_enqueue_style( "accelerate-child-google-fonts", "//fonts.googleapis.com/css?family=Spicy+Rice" );
 }
 add_action( 'wp_enqueue_scripts', 'accelerate_child_scripts' );
 
@@ -33,3 +34,18 @@ function create_custom_post_types() {
     );
 }
 add_action( 'init', 'create_custom_post_types' );
+
+function accelerate_theme_child_widget_init() {
+    
+    register_sidebar( array(
+        'name' =>__( 'Homepage sidebar', 'accelerate-theme-child'),
+        'id' => 'sidebar-2',
+        'description' => __( 'Appears on the static front page template', 'accelerate-theme-child' ),
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+    ) );
+    
+}
+add_action( 'widgets_init', 'accelerate_theme_child_widget_init' );
